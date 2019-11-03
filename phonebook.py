3329fab0e22c1e0513c8ed48c6745d69e4658968
AJuszczakiewicz <ajuszczakiewicz@protonmail.com>
2019-11-03 08:02:35 +0100

    Add number to existing contact feature

--- phonebook.py
+++ phonebook.py
@@ -1,3 +1,4 @@
+
 class Phonebook:
     def __init__(self):
         self.contact_list = []
@@ -14,5 +15,15 @@ class Phonebook:
         contact = self.get_contact_by_contact_id(contact_id)
         del contact
 
+    def add_number_to_existing_contact(self, contact_id, phone_number, phone_type):
+        contact = self.get_contact_by_contact_id(contact_id)
+        contact.add_new_number(phone_number, phone_type)
+
     def __repr__(self):
         return self.contact_list
+
+    def __str__(self):
+        repr_str = ""
+        for contact in self.contact_list:
+            repr_str = repr_str + str(contact) + "\n"
+        return repr_str
